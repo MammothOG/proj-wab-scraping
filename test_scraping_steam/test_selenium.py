88c991edbf07cdfb32b7d908dd8d60615c040902
MammothOG <felix.capuano@gmail.com>
2020-05-01 18:16:57 +0200

    selenium thread working well

--- test_scraping_steam/test_selenium.py
+++ test_scraping_steam/test_selenium.py
@@ -1,31 +1,105 @@
-from selenium import webdriver
-# from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
+import urllib.parse
+from threading import Thread, Lock
+
+import pandas as pd
+from selenium import webdriver
+
+class DataPoller(Thread):
+    def __init__(self, name, condition=None, timer=0, refresh=10):
+        Thread.__init__(self)
+        self.lock = Lock()
+        self.running = False
+
+        self.game_id = 730
+        self.item_name = name
+        self.condition = condition
+
+        self.start_time = 0
+        self.timer = timer
+        self.refresh = refresh
+
+        # url_name = "{} ({})".format(self.item_name, self.condition)
+        url_name = "{}".format(self.item_name)
+        parsed_name = urllib.parse.quote(url_name)
+        self.adresse = "https://steamcommunity.com/market/listings/{game_id}/{item_name}" \
+                        .format(game_id=self.game_id, item_name=parsed_name)
+        
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(20)
+        self.driver.get(self.adresse)
+
+        self.buf = []
 
 
-refresh_time = 10
+    timer_check = lambda self: time.time() - self.start_time > self.timer
 
-driver = webdriver.Chrome()
-driver.get("https://steamcommunity.com/market/listings/730/Shattered%20Web%20Case")
+    def run(self):
+        counter = 0
+        self.running = True
+        self.start_time = time.time()
+        try:
+            while True:
+                if not self.running or self.timer_check():
+                    self.stop()
+                    break
+                
+                with self.lock:
+                    self.scrap_data()
 
-assert "Marché de la communauté Steam :: Offres pour Shattered Web Case" in driver.title
+                time.sleep(self.refresh)
+            
+        finally:
+            self.driver.close()
+            print("close driver")
 
-i = 0
+    def stop(self):
+        with self.lock:
+            self.running = False
+            print("stop thread")
 
-try:
-    wait  = WebDriverWait(driver, refresh_time)
-    for i in range(10):
-        time.sleep(10)
-        element = wait.until(EC.presence_of_element_located((By.CLASS_NAME,
-                    "market_commodity_orders_header_promote"))
-        )
-        print(element.text)
-finally:
+    def scrap_data(self):
 
-    assert "No results found." not in driver.page_source
-    driver.close()
+        # building link to locate price and quantity of  items
+        sale = "market_commodity_buyrequests"
+        xpath_quantity = "//div[@id='{}']/span[1]".format(sale)
+        xpath_price = "//div[@id='{}']/span[2]".format(sale)
+
+        # get element
+        quantity_element = self.driver.find_element_by_xpath(xpath_quantity)
+        price_element = self.driver.find_element_by_xpath(xpath_price)
+
+        # clean element 
+        time_stamp = int(time.time())
+        quantity = int(quantity_element.text)
+        price = float(price_element.text.replace("$", ""))
+
+        self.buf.append([time_stamp, quantity, price])
+
+    def get_data_frame(self):
+        with self.lock:
+            print("get data")
+            col = ["time", "quantity", "price"]
+            data_frame = pd.DataFrame(data=self.buf, columns=col)
+
+            self.buf = []
+            
+            return data_frame
 
 
+
+if __name__ == "__main__":
+    poller_prisma = DataPoller("Prisma 2 Case", timer=60)
+    poller_phoenix = DataPoller("Operation Phoenix Weapon Case", timer=60)
+
+    poller_prisma.start()
+    poller_phoenix.start()
+
+    while poller_prisma.running and poller_phoenix.running:
+        time.sleep(2)
+
+    df_prisma = poller_prisma.get_data_frame()
+    df_phoenix = poller_phoenix.get_data_frame()
+    
+    df_prisma.to_csv("prisma.csv")
+    df_phoenix.to_csv("phoenix.csv")
